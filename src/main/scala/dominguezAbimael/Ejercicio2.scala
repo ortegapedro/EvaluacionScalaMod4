ae51a03fed9846593ca9c08f805a40cc44c7d74a
Abimael <abimael.dominguez.perez@gmail.com>
2022-08-13 12:50:25 -0500

    add exceptions

--- src/main/scala/dominguezAbimael/Ejercicio2.scala
+++ src/main/scala/dominguezAbimael/Ejercicio2.scala
@@ -2,11 +2,13 @@ package main.scala.dominguezAbimael
 
 import scala.annotation.tailrec
 
-object Ejercicio2 extends App{
+object Ejercicio2 extends App {
 
   var n: Int = 0
+    def countCharacters(word: String): Int = {
 
-  def countCharacters(word: String): Int = {
+      if (word == null) throw new NullPointerException("Your Sting is Null")
+      if  (word == "") throw new NullPointerException("Your Sting is Empty")
 
       if (word.length < 1)
         1
@@ -15,7 +17,16 @@ object Ejercicio2 extends App{
         n = n + 1
         n
       }
-  }
+    }
+
+
     println(countCharacters("Hellos"))
+    //println(countCharacters(null))
+    //println(countCharacters(""))
+
+
+
+
+
 
 }
