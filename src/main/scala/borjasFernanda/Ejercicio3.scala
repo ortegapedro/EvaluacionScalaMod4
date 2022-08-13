66f6eb40c9f967dc7279afb7bb820c9362f0d842
Feractal <fernandaborjas@ciencias.unam.mx>
2022-08-13 13:57:25 -0500

    Update

--- src/main/scala/borjasFernanda/Ejercicio3.scala
+++ src/main/scala/borjasFernanda/Ejercicio3.scala
@@ -7,9 +7,15 @@ object Ejercicio3 extends App{
   case class Tablet(DNI:String, Modelo:String) extends Disp
 
   def imprimirCaracteristicas(e: Disp): String = e match {
-    case Celular(i,s,m) if m == "iPhone" => s"IMEI $i SIM $s MARCA $m"
-    case Laptop(s,v) =>  s"SERIE $s VERSION $v"
-    case Tablet(d,m) => s"DNI $d MODELO $m"
-    case _ => throw new RuntimeException ("DispositivoException")
+    case Celular(i, s, m) if m == "iPhone" => s"SIM: $s IMEI: $i"
+    case Laptop(s, v) => s"SERIE: $s VERSION: $v"
+    case Tablet(d, m) => s"DNI: $d MODELO: $m"
+    case _ => throw new RuntimeException("DispositivoException")
   }
-  }
+
+  val someCelular = Celular("12345", "565", "iPhone")
+  val someTablet = Tablet("2342", "38687")
+
+  println(imprimirCaracteristicas(someCelular))
+  println(imprimirCaracteristicas(someTablet))
+}
---
+++ src/main/scala/borjasFernanda/Ejercicio4.scala
@@ -0,0 +1,8 @@
+package main.scala.borjasFernanda
+
+object Ejercicio4 {
+  case class Ixtlilton(Encendido: Boolean, otherParams: String*)
+  case class Yacatecuhtli(Encendido: Boolean, otherParams: String*)
+  case class Quetzalcoatl(Encendido: Boolean, otherParams: String*)
+
+}
